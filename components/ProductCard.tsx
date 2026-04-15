81760444e1fd0263627e7d3843be32ff9088e7c6
Adry <aaguileram@est.emi.edu.bo>
2026-04-15 19:20:46 -0400

    Add responsive image

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,20 +1,27 @@
-import Image from "next/image";
 import PriceSection from "@/components/PriceSection";
 import ProductDetails from "@/components/ProductDetails";
 import CartButton from "@/components/CartButton";
 import "@/components/ProductCard.css";
 
 const ProductCard = () => {
-    const imagenproducto = "/image-product-mobile.jpg";
     return (
         <div className="productcontainer">
             <div className="imagecontainer">
-                <Image
-                    src={imagenproducto}
-                    alt="Imagen del producto"
-                    fill
-                    style={{ objectFit: "cover" }}
-                />
+                <picture>
+                    <source
+                        srcSet="/image-product-desktop.jpg"
+                        media="(min-width: 1440px)"
+                    />
+                    <img
+                        src="/image-product-mobile.jpg"
+                        alt="Imagen del producto"
+                        style={{
+                            width: "100%",
+                            height: "100%",
+                            objectFit: "cover",
+                        }}
+                    />
+                </picture>
             </div>
             <div className="texto">
                 <ProductDetails />
