6f95b0570aa638efda94e951c53568b2474f09e4
Adry <aaguileram@est.emi.edu.bo>
2026-04-15 19:09:03 -0400

    Add responsive desktop

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,20 +1,28 @@
 import Image from "next/image";
-import ContentDescription from "@/components/ContentDescription";
+import PriceSection from "@/components/PriceSection";
+import ProductDetails from "@/components/ProductDetails";
+import CartButton from "@/components/CartButton";
 import "@/components/ProductCard.css";
 
 const ProductCard = () => {
-  const imagenproducto = "/image-product-mobile.jpg";
-  return (
-    <div className="productcontainer">
-      <Image
-        src={imagenproducto}
-        alt="Imagen del producto"
-        width={350}
-        height={350}
-      />
-      <ContentDescription />
-    </div>
-  );
+    const imagenproducto = "/image-product-mobile.jpg";
+    return (
+        <div className="productcontainer">
+            <div className="imagecontainer">
+                <Image
+                    src={imagenproducto}
+                    alt="Imagen del producto"
+                    fill
+                    style={{ objectFit: "cover" }}
+                />
+            </div>
+            <div className="texto">
+                <ProductDetails />
+                <PriceSection />
+                <CartButton />
+            </div>
+        </div>
+    );
 };
 
 export default ProductCard;
